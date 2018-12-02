3c71a03b16350609bcb5485e4cec202e01653ee5
Akhilesh Amle <mac@macs-macbook-pro.local>
2018-12-02 23:12:26 +0530

    Created empty class for insertion sort.

---
+++ theSwiftAlgorithmClub_iOS/Sorting/InsertionSort.swift
@@ -0,0 +1,17 @@
+//  InsertionSort.swift
+//  theSwiftAlgorithmClub_iOS
+//  Created by Akhilesh Amle on 02/12/18.
+//  Copyright © 2018 AkhileshAmle. All rights reserved.
+
+import UIKit
+
+class InsertionSort: NSObject {
+
+    public func insertionSortOperation() {
+        self.insertionSort()
+    }
+    
+    private func insertionSort() {
+        
+    }
+}
